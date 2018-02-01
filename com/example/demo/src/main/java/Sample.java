c35dc6d141e655c0590d1ee304e2c528b0bcf670
遊歩 <yuho.ito@walker.co.jp>
2018-02-01 22:17:11 +0900

    サンプルテストコードの追加

---
+++ com/example/demo/src/main/java/Sample.java
@@ -0,0 +1,8 @@
+/**
+ * Created by yuho on 2018/02/01.
+ */
+public class Sample {
+    public static int num() {
+        return 11;
+    }
+}
---
+++ com/example/demo/src/test/java/SampleTest.java
@@ -0,0 +1,12 @@
+/**
+ * Created by yuho on 2018/02/01.
+ */
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class SampleTest {
+    @Test
+    public void testNum() {
+        assertEquals("is not 10!!",10, Sample.num());
+    }
+}
